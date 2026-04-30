44f02aaeeb06d87757762319cb96d4de22f4a745
Lee9Bin <imagen33@daum.net>
2026-05-01 02:39:49 +0900

    feat: 내 랭킹 스냅샷 조회 메서드 추가

--- back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/repository/RankSnapshotEntryRepository.java
+++ back/tikkle/src/main/java/com/taesan/tikkle/domain/rank/repository/RankSnapshotEntryRepository.java
@@ -1,5 +1,6 @@
 package com.taesan.tikkle.domain.rank.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -12,6 +13,8 @@ import com.taesan.tikkle.domain.rank.entity.RankSnapshotEntry;
 public interface RankSnapshotEntryRepository extends JpaRepository<RankSnapshotEntry, UUID> {
 
 	Page<RankSnapshotEntry> findByRankSnapshotOrderByPositionAsc(RankSnapshot rankSnapshot, Pageable pageable);
+
+	Optional<RankSnapshotEntry> findByRankSnapshotAndMemberId(RankSnapshot rankSnapshot, UUID memberId);
 
 	Page<RankSnapshotEntry> findByRankSnapshotAndMemberNameContainingOrderByPositionAsc(
 		RankSnapshot rankSnapshot,
